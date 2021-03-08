4524bbb3f630330a5f7bf91f1abf59007875e2be
erika-tyagi <erika.tyagi@gmail.com>
2021-03-08 07:44:27 -0600

    update: add initial dashboard prototype

---
+++ server.R
@@ -0,0 +1,111 @@
+library(remotes)
+library(shiny)
+library(tidyverse)
+library(behindbarstools)
+library(plotly)
+library(scales)
+library(stringr)
+
+shinyServer(function(input, output) {
+    
+    withProgress(message = "Loading Data...", style = "old", {
+        scrape_ca <- read_scrape_data(all_dates = TRUE, state = "California") %>%
+            mutate(Residents.Confirmed.Rate = Residents.Confirmed / Residents.Population,
+                   Residents.Deaths.Rate = Residents.Deaths / Residents.Population,
+                   Residents.Active.Rate = Residents.Active / Residents.Population,
+                   Residents.Tadmin.Rate = Residents.Tadmin / Residents.Population)
+    })
+    
+    output$plot <- renderPlotly(
+        {getPlot(scrape_ca, input$facility, input$metric, input$population)})
+})
+
+getPlot <- function(df, fac_name, metric, population){
+    
+    variable <- getMetric(metric, population)
+    
+    if (str_ends(variable, ".Rate")){
+        plt <- df %>% 
+            filter(!is.na(!!sym(variable))) %>%
+            filter(Name == fac_name) %>%
+            ggplot(aes(x = Date, y = !!sym(variable), group = 1, 
+                       text = sprintf("Date: %s<br>%s: %s", 
+                                      Date, 
+                                      metric, 
+                                      percent(!!sym(variable), accuracy = 0.1)))) +
+            geom_line(size = 1, color = "#D7790F") +
+            labs(title = str_c(metric, " Among ", population, "\n", fac_name)) + 
+            scale_x_date(date_labels = "%b %Y") + 
+            scale_y_continuous(labels = percent_format(accuracy = 0.1)) + 
+            customTheme
+    } else {
+        plt <- df %>% 
+            filter(!is.na(!!sym(variable))) %>%
+            filter(Name == fac_name) %>%
+            ggplot(aes(x = Date, y = !!sym(variable), group = 1, 
+                       text = sprintf("Date: %s<br>%s: %s", 
+                                      Date, 
+                                      metric, 
+                                      comma(!!sym(variable), accuracy = 1)))) +
+            geom_line(size = 1, color = "#D7790F") +
+            labs(title =  str_c(metric, " Among ", population, "\n", fac_name)) + 
+            scale_x_date(date_labels = "%b %Y") + 
+            scale_y_continuous(labels = comma_format(accuracy = 1)) + 
+            customTheme
+    }
+    
+    font <- list(
+        family = "Helvetica",
+        size = 15,
+        color = "black"
+    )
+    
+    label <- list(
+        bgcolor = "#EFEEEC",
+        bordercolor = "transparent",
+        font = font
+    )
+    
+    print(ggplotly(plt, tooltip = "text") %>% 
+              style(hoverlabel = label) %>%
+              layout(font = font)
+    )
+}
+
+getMetric <- function(metric, population){
+    lookup_residents <- c(
+        "Cumulative Cases" = "Residents.Confirmed", 
+        "Cumulative Case Rate" = "Residents.Confirmed.Rate", 
+        "Cumulative Deaths" = "Residents.Deaths", 
+        "Cumulative Death Rate" = "Residents.Deaths.Rate", 
+        "Active Cases" = "Residents.Active", 
+        "Active Case Rate" = "Residents.Active.Rate", 
+        "Tests Administered" = "Residents.Tadmin", 
+        "Testing Rate" = "Residents.Tadmin.Rate", 
+        "Population" = "Residents.Population"
+    )
+    
+    lookup_staff <- c(
+        "Cumulative Cases" = "Staff.Confirmed", 
+        "Cumulative Deaths" = "Staff.Deaths"
+    )
+        
+    if (population == "Incarcerated People"){
+        lookup_residents[metric] %>% 
+            unname()
+    
+    } else if (population == "Staff"){
+        lookup_staff[metric] %>% 
+            unname()
+    }
+}
+
+customTheme <- 
+    theme_classic(base_size = 12) +     
+    theme(axis.title.y = element_blank(), 
+          axis.title.x = element_blank(), 
+          plot.title = element_text(hjust = 0.5), 
+          panel.grid.major.y =  element_line(),
+          axis.line.y = element_blank(),
+          axis.ticks.y = element_blank()
+    )
---
+++ ui.R
@@ -0,0 +1,63 @@
+library(shiny)
+library(bslib)
+library(plotly)
+library(behindbarstools)
+
+METRICS <- c(
+    "Cumulative Cases", 
+    "Cumulative Case Rate", 
+    "Cumulative Deaths", 
+    "Cumulative Death Rate", 
+    "Active Cases", 
+    "Active Case Rate", 
+    "Tests Administered", 
+    "Testing Rate", 
+    "Population"
+)
+
+POPULATIONS <- c(
+    "Incarcerated People", 
+    "Staff"
+)
+
+shinyUI(fluidPage(
+    
+    theme = bs_theme(
+        base_font = "Helvetica", 
+        primary = "#D7790F"
+    ), 
+    
+    tags$style(type = "text/css", ".selectize-input {font-size: 14px;} .selectize-dropdown {font-size: 14px}"),
+
+    titlePanel(
+        h1("UCLA Law COVID-19 Behind Bars Data Project", align = "center"), 
+        windowTitle = "COVID Behind Bars Dashboard"
+    ), 
+    fluidRow(
+        column(12, p("This is a work-in-progress internal dashboard used by the", 
+        a("UCLA Law COVID-19 Behind Bars", href = "https://uclacovidbehindbars.org/"), "Data Team.",  
+                     align = "center"))
+    ), 
+    fluidRow(
+        column(6, selectInput("facility", "Facility", 
+                              choices = read_fac_info() %>% 
+                                  filter(State == "California") %>% 
+                                  filter(Jurisdiction == "state") %>% 
+                                  select(Name), 
+                              width = "100%")),
+        column(3, selectInput("metric", 
+                              "Metric", 
+                              choices = METRICS, 
+                              width = "100%")), 
+        column(3, selectInput("population", 
+                              "Population", 
+                              choices = POPULATIONS, 
+                              width = "100%"))
+    ), 
+    fluidRow(
+        column(12, plotlyOutput("plot", height = "500px"))
+    ), 
+    fluidRow(column(12, align = "center", 
+                    tags$footer(a("Dashboard Source Code", href = "https://github.com/uclalawcovid19behindbars/ts-dashboard"), " | ", 
+                                a("Data Repository", href = "https://github.com/uclalawcovid19behindbars/data"))))
+))
